1dfe2fe021fd548ba036ecfc34c58f4c5a75c1a1
ummet <ummetaydin080@gmail.com>
2022-12-22 18:10:06 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let sayi=prompt("Lütfen 1 ile 5 arasında deger giriniz")
+switch (sayi) {
+    case "1":
+        console.log("girilen sayi 1 dir");
+        break;
+case "2":
+    console.log("girilen sayi 2 dir");
+    break
+    case "3":
+        console.log("girilen sayi 3 tür");
+    break
+    case"4":
+    console.log("girilen sayi 4 tür");
+    break
+case "5":
+    console.log("girilen sayi 5 dir");
+    break
+    default:
+        console.log("girilen sayi 1 ile 5 arasında olmalıdır");
+        break;
+}
